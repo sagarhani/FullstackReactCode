5fe51448683922a007306c78ebd1920432319448
Sagar S <Sagar.S@betsol.com>
2017-10-14 08:50:39 +0530

    Finished auth flow 😃

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,8 @@
-module.exports = {
-  googleClientID: '186684571978-fgdo74ulbg9obh2md7o9ho35dhut1bmg.apps.googleusercontent.com',
-  googleClientSecret: 'RIy_ntx6wSFl7jVbPnhFv6hn'
+// keys.js - figure out what set of credentials to return
+if (process.env.NODE_ENV === 'production') {
+  // we are in production - return the prod set of keys
+  module.exports = require('./prod');
+} else {
+  // we are in development - return the dev keys
+  module.exports = require('./dev');
 }
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,20 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    })
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,8 +24,20 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('Access Token : ', accessToken);
-      console.log('Refresh Token : ', refreshToken);
-      console.log('Profile : ', profile);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          //we already have a recode with given profile ID
+          console.log("User Exist!");
+          done(null, existingUser);
+        } else {
+          //we don't have a user record with this ID, make a new record
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => {
+            done(null, user);
+          });
+        }
+      });
     }
-  ));
+  )
+);
